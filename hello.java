246be6530b8c9aa2d956340ccf04ee32e1094de2
YashrajPanda <yashrajpanda320@gmail.com>
2025-12-03 05:12:18 +0000

    Variables in java

---
+++ hello.java
@@ -0,0 +1,11 @@
+// To run the below code : 
+// 1. Save the code in a file named `hello.java`
+// 2. Open a terminal and navigate to the directory where the file is saved
+// 3. Compile the Java file using the command: javac hello.java
+// 4. Run the compiled class using the command: java Hello
+class Hello {
+
+    public static void main(String a[]) {
+        System.out.println("Hello, World!");
+    }
+}
---
+++ variable.java
@@ -0,0 +1,23 @@
+// Varibales in Java
+class Variable {
+    public static void main(String[] args) {
+        // Declare an integer variable
+        int myNumber = 10;
+        
+        // Declare a string variable
+        String myText = "Hello, Java!";
+        
+        // Print the variables
+        System.out.println("Integer: " + myNumber);
+        System.out.println("String: " + myText);
+    }
+}
+
+// Anotehr varibales in Java
+// class VariableExample {
+//     public static void main(String[] args) {
+//         // Declare and initialize variables
+//         int age = 25;
+//         double height = 5.9;
+//         char grade = 'A';
+//         boolean isStudent = true;
