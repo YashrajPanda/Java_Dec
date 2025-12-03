e3ebffd0a3ab34ddabb7524e40c09c8fe24ba97b
YashrajPanda <yashrajpanda320@gmail.com>
2025-12-03 06:49:24 +0000

    Literals in java

---
+++ Literal.java
@@ -0,0 +1,30 @@
+// Literal in java
+
+class Literal {
+
+    public static void main(String args[]) {
+        // Integer Literal
+        int intLiteral = 100;
+        System.out.println("Integer Literal: " + intLiteral);
+
+        // Floating Point Literal
+        float floatLiteral = 10.5f;
+        System.out.println("Floating Point Literal: " + floatLiteral);
+
+        // Character Literal
+        char charLiteral = 'A';
+        System.out.println("Character Literal: " + charLiteral);
+
+        // String Literal
+        String stringLiteral = "Hello, Java!";
+        System.out.println("String Literal: " + stringLiteral);
+
+        // Boolean Literal
+        boolean boolLiteral = true;
+        System.out.println("Boolean Literal: " + boolLiteral);
+
+        // Null Literal
+        String nullLiteral = null;
+        System.out.println("Null Literal: " + nullLiteral);
+    }
+}
