629811488e63d383742eacfd0dc13138f62c468e
YashrajPanda <yashrajpanda320@gmail.com>
2025-12-03 06:45:45 +0000

    datatypes in java

---
+++ DataTypes.java
@@ -0,0 +1,18 @@
+// Datatypes in Java
+
+class Data{
+    public static void main(String a[]){
+        // byte b = 5;
+        // System.out.println("Byte value: " + b);
+
+        // int num1 = 9;
+        // System.out.println("Integer value: " + num1);
+
+        byte b = 127; // This will cause a compilation error
+        short sh = 558;
+        long l = 5854l;
+        float f = 5.8f;
+        // System.out.println("Byte value: " + b);
+
+    }
+}
