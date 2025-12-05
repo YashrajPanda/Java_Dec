f514a9ead222fb6845a585caf1445a85918b43b5
YashrajPanda <yashrajpanda320@gmail.com>
2025-12-05 16:28:10 +0000

    consditional statements in java

---
+++ ifelse.java
@@ -0,0 +1,27 @@
+// IF ELSE Statement Example in Java
+
+class IFELSE{
+    public static void main(String args[]){
+        // int n = 10;
+        // if(n<5){
+        //     System.out.println("hello");
+        // }else{
+        //     System.out.println("bye");
+        // }
+
+        int x = 8;
+        int y = 7;
+        int z = 9;
+
+        if(x>y && x>z){
+            System.out.println("x is greatest");
+        }
+        else if(y>z){
+            System.out.println("y is greatest");
+        }
+        else{
+            System.out.println("z is greatest");
+        }
+    }
+}
+
