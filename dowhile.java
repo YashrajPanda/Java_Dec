4c5c1fc7c44fc75777ba650b17e24a7411061449
YashrajPanda <yashrajpanda320@gmail.com>
2025-12-06 18:30:14 +0000

    do while loops in java

---
+++ dowhile.java
@@ -0,0 +1,12 @@
+// do while loops in java
+
+public class dowhile{
+    public static void main(String a[]){
+        int i = 1;
+        // print numbers from 1 to 5 using do while loop
+        do{
+            System.out.println("Hello " + i);
+            i++;
+        } while(i <= 5);
+    }
+}
