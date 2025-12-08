5151b55e5bfb1f15c07f236b477079d50c4160d2
YashrajPanda <yashrajpanda320@gmail.com>
2025-12-08 17:45:39 +0000

    strings in java

---
+++ StringExample.java
@@ -0,0 +1,12 @@
+public class StringExample {
+    public static void main(String[] a) {
+        String str = "Yash";
+        str = str + " Raj";
+        System.out.println("hello " + str);
+
+        String s1 = "Navin";
+        String s2 ="Navin";
+
+        System.out.println(s1 == s2); // true
+    }
+}
